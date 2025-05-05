a10ebf0ff49b7de0c8457bc071d48f7e56a0eb34
g3ntl3 <hammedlawal412@gmail.com>
2025-05-06 00:09:47 +0100

    Was able to complete Sign up and Log in finally 🍻 Moviing to displaying username on the dashboard

--- login/login.js
+++ login/login.js
@@ -18,27 +18,53 @@ const toast = (text, backgroundcolor, textColor) => {
 
 var savedData = JSON.parse(localStorage.getItem('savedata')) || []
 
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,}$/;
 
 
-let isUserExist 
+
+let isUserExist
 
 const logIn = () => {
+    login.innerHTML = ' ...loading'
 
-     isUserExist = (savedData.find(data => data.Password == loginPassword.value && data.Email == loginEmail.value))
+    isUserExist = (savedData.find(data => data.Password == loginPassword.value && data.Email == loginEmail.value))
 
-    if (isUserExist) {
-        toast('Log in successful', 'green', 'white');
 
-        
-        window.location.href = '../temu.html'
-        // alert('Welcome')
+    if (loginEmail.value == "" || loginPassword.value == '') {
+        setTimeout(() => {
+
+            login.innerHTML = 'Log in'
+            toast('Please complete the form', 'red', 'white')
+        }, 1000)
+
+    } else if (!emailRegex.test(loginEmail.value)) {
+        toast('Please enter a valid email address', 'red', 'white')
+    } else if (!passwordRegex.test(loginPassword.value)) {
+        toast('Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character', 'red', 'white')
+    }
+
+
+
+    else if (isUserExist) {
+
+        setTimeout(() => {
+            toast('Log in successful', 'green', 'white');
+            window.location.href = '../temu.html'
+
+        }, 2000)
+
 
     }
 
     else {
-        // alert('wrongpassword')
-        toast('Wrong password', 'red', 'white');
-        // alert('failed')  
+        setTimeout(() => {
+
+            login.innerHTML = 'Log in'
+            toast('Wrong password or Email', 'red', 'white');
+        }, 1000)
+
+
     }
 
 
--- signup/Signup.js
+++ signup/Signup.js
@@ -21,39 +21,29 @@ const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@
 
 let detailsArray = JSON.parse(localStorage.getItem('savedata')) || []
 
-
 const signUp = () => {
-// let detailsobj = {
-//     fullName: fullName.value,
-//     userName: userName.value,
-//     Email: email.value,
-//     Password: password.value,
-
-// }
-
-// submit.innerHTML='...loading'
+    let detailsobj = {
+        fullName: fullName.value,
+        userName: userName.value,
+        Email: email.value,
+        Password: password.value
+    }
 
     if (fullName.value == "" || userName.value == "" || email.value == "" || password.value == '') {
         toast('Please complete the form', 'red', 'white')
-
+    } else if (!emailRegex.test(email.value)) {
+        toast('Please enter a valid email address', 'red', 'white')
+    } else if (!passwordRegex.test(password.value)) {
+        toast('Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character', 'red', 'white')
     } else {
-        alert('form complete')
-        // // if (!emailRegex.test(email.value)) {
-        // //     toast('Please enter a valid email address', 'red', 'white')
-        // // } else if (!passwordRegex.test(password.value)) {
-        // //     toast('Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character', 'red', 'white')
-        // // } else {
-        // //     fullName.value = ''
-        // //     userName.value = ''
-        // //     email.value = ''
-        // //     password.value = ''
-        // //     detailsArray.push(detailsobj);
-        // //     savedDate = localStorage.setItem('savedata', JSON.stringify(detailsArray))
-        // //     toast('Sign up successful', 'green', 'white')
-        // //     window.location.href='../login.html'
-
-
-        // }
+        detailsArray.push(detailsobj);
+        localStorage.setItem('savedata', JSON.stringify(detailsArray));
+        toast('Sign up successful', 'green', 'white')
+        fullName.value = '';
+        userName.value = '';
+        email.value = '';
+        password.value = '';
+        window.location.href = '../login/login.html';
     }
 };
 // console.log(detailsArray.find('O8fcjhkdbvijh2.  '));
